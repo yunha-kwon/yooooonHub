afe882a19148a939cf0cea1cc0f0ac04660fabff
권뉴나 <152950017+yunha-kwon@users.noreply.github.com>
2024-01-27 21:35:09 +0900

    [Bronze I] Title: 세로읽기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,9 +1,9 @@
 word_lst = []
+
 for i in range(5):
     word_lst.append(list(input()))
-#print(word_lst)
 
-for j in range(15):
-    for i in range(5):
-        if j < len(word_lst[i]):
-            print(word_lst[i][j],end='')
+for i in range(15):
+    for j in range(5):
+        if i < len(word_lst[j]):
+            print(word_lst[j][i], end='')
