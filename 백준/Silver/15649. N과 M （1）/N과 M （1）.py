d2fa5f5b05b55b835a5c21f3122c7e12614eba43
권뉴나 <152950017+yunha-kwon@users.noreply.github.com>
2024-02-22 20:26:33 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 121836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,23 @@
-def NandM(i, n, m): # i: answer 배열에 들어온 숫자의 개수
-    if i == m:
-        print(*answer)
-    for j in range(1, n+1):
-        if not visited[j]:
+#가능한 모든 경우 실행
+#n: 선택된 숫자의 개수(길이)
+#구해야 할 것: 길이 M짜리 수열
+
+def dfs(n, lst):
+    #종료조건
+    if n == M:
+        ans.append(lst)
+        return ans
+
+    for j in range(1, N+1):
+        if visited[j] == 0:
             visited[j] = 1
-            answer.append(j)
-            NandM(i+1, n, m)
+            dfs(n+1, lst+[j])
             visited[j] = 0
-            answer.pop()
 
-n, m = map(int, input().split())
-visited = [0] * (n+1)
-answer = []
 
-NandM(0, n, m)
-
+N, M = map(int, input().split()) #1~N 자연수 중 중복 없이 M개 고른 수열 (사전 순으로 증가하게 출력)
+ans = [] #정답 리스트를 저장할 리스트
+visited = [0] * (N+1) #중복을 확인할 리스트
+dfs(0, [])
+for lst in ans:
+    print(*lst)
