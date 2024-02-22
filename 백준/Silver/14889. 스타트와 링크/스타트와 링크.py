d267653718ea94ca1d2e675e82323b695209faa5
권뉴나 <152950017+yunha-kwon@users.noreply.github.com>
2024-02-22 22:00:49 +0900

    [Silver I] Title: 스타트와 링크, Time: 508 ms, Memory: 114692 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,31 +1,30 @@
-#백트랙킹... 여전히 어렵다
-import sys
-input = sys.stdin.readline
+# 백트래킹: 가능한 모든 경우
+# 스타트와 링크
+# n: 사람 번호(0번 ~ N-1번)
+def calc(a_lst, b_lst):
+    a_sum = b_sum = 0
+    for i in range(M):
+        for j in range(M):
+            a_sum += team[a_lst[i]][a_lst[j]]
+            b_sum += team[b_lst[i]][b_lst[j]]
+    return abs(a_sum - b_sum)
 
-def combi(x, n):
-    global result
-    if x == N // 2:
-        s, l = 0, 0 #스타트, 링크
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    s += S[i][j]
-                elif not visited[i] and not visited[j]:
-                    l += S[i][j]
-
-        result = min(result, abs(s-l))
+def dfs(n, a_lst, b_lst):
+    global ans
+    # 종료조건
+    if n == N:
+        if len(a_lst) == len(b_lst): # 같은 인원 수로 팀을 구성
+            ans = min(ans, calc(a_lst, b_lst))
         return
 
-    else:
-        for i in range(n, N):
-            if visited[i] == 0:
-                visited[i] += 1
-                combi(x+1, i+1)
-                visited[i] = 0
+    dfs(n+1, a_lst+[n], b_lst) # A팀 선택한 경우
+    dfs(n+1, a_lst, b_lst+[n]) # B팀 선택한 경우
+
 
 N = int(input())
-S = [list(map(int, input().split())) for _ in range(N)]
-visited = [0] * N
-result = 100000
-combi(0, 0)
-print(result)
+team = [list(map(int, input().split())) for _ in range(N)]
+
+M = N // 2
+ans = 100*M*M
+dfs(0, [], [])
+print(ans)
