0b778b4e9ef1dc29bd6fe7a9cec98c68e0d373e8
권뉴나 <152950017+yunha-kwon@users.noreply.github.com>
2024-03-31 22:46:47 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -0,0 +1,14 @@
+N = int(input())
+t = list(map(int, input().split()))
+y = 0
+m = 0
+for i in t:
+    y += (i // 30 + 1) * 10
+    m += (i // 60 + 1) * 15
+
+if y > m:
+    print('M', m)
+elif y < m:
+    print('Y', y)
+else:
+    print('Y M', y)
